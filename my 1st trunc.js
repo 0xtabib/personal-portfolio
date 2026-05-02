c3299d82543d89eb9b817f4f19f99289b7c08b3e
0xtabib <talhathankspro@gmail.com>
2026-05-02 11:18:00 +0600

    Create my 1st trunc.js

---
+++ my 1st trunc.js
@@ -0,0 +1,9 @@
+let x = 15.5;
+let fullnumber= Math.trunc.(x);
+console.log (`the flot number is ${x}`);
+console.log (`the real number is ${fullnumber}`)
+
+//RESULT
+//the flot number is 15.5
+//the real number is 15
+
